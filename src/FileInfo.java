b4bec7c68febb3c882129d3f8405dc5669bdcbe1
Partha Priyadarshi Das <66919291+MrSnor@users.noreply.github.com>
2024-01-04 13:12:21 +0530

    format main java files
    using redhat java formatter

--- src/FileInfo.java
+++ src/FileInfo.java
@@ -2,8 +2,7 @@ import java.io.Serializable;
 
 @SuppressWarnings("serial")
 
-public class FileInfo implements Serializable
-{
+public class FileInfo implements Serializable {
 	public int peerid;
 	public String fileName;
 	public int portNumber;
