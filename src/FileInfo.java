4ba71906a7af434213d4b8a039e07bc69503993a
Partha Priyadarshi Das <66919291+MrSnor@users.noreply.github.com>
2024-01-06 09:58:29 +0530

    add constructors to FileInfo.java

--- src/FileInfo.java
+++ src/FileInfo.java
@@ -2,8 +2,30 @@ import java.io.Serializable;
 
 @SuppressWarnings("serial")
 
+/**
+ * Represents information about a file.
+ */
 public class FileInfo implements Serializable {
 	public int peerid;
 	public String fileName;
 	public int portNumber;
+
+	/**
+	 * Default constructor for FileInfo.
+	 */
+	public FileInfo() {
+	}
+
+	/**
+	 * Constructor for FileInfo.
+	 * 
+	 * @param fileName   The name of the file.
+	 * @param peerid     The ID of the peer.
+	 * @param portNumber The port number.
+	 */
+	public FileInfo(String fileName, int peerid, int portNumber) {
+		this.peerid = peerid;
+		this.fileName = fileName;
+		this.portNumber = portNumber;
+	}
 }
